b991c8158d5310df7e2ad415f8d940b7534873c5
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2023-06-20 01:00:08 +0900

    Add progress bar components and examples.

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script setup>
+import ProgressBar from "./components/ProgressBar.vue";
+import ProgressBarItem from "./components/ProgressBarItem.vue";
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <progress-bar>
+      <progress-bar-item
+        color="yellow"
+        visited="1"
+        type="castle"
+      ></progress-bar-item>
+      <progress-bar-item
+        color="green"
+        visited="3"
+        type="view"
+      ></progress-bar-item>
+    </progress-bar>
   </div>
 </template>
---
+++ components/ProgressBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="progress-bar">
+    <div class="progress-start"></div>
+    <slot></slot>
+    <div class="progress-placeholder"></div>
+    <div class="progress-end"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.progress-bar {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+}
+.progress-start {
+    display: inline-block;
+    height: 30px;
+    width: 15px;
+    border-radius: 15px 0 0 15px;
+    background-color: #333;
+}
+.progress-placeholder {
+    display: inline-block;
+    height: 26px;
+    width: 20%;
+    border-top: 2px solid #333;
+    border-bottom: 2px solid #333;
+    background-color: #ccc;
+}
+.progress-end {
+    display: inline-block;
+    height: 30px;
+    width: 15px;
+    border-radius: 0 15px 15px 0;
+    background-color: #333;
+}
+</style>
