af2c54cf3c455840234151877783784230c00c1e
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2023-06-20 23:06:20 +0900

    Fix nuxt config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import type { NuxtConfig } from '@nuxt/type'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  buildModules: ['@nuxt/typescript-build'],
 })
