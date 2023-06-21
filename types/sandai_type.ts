44e6356cd42fcd7e22311b03457db6b9820fe3f3
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2023-06-21 23:55:19 +0900

    Add sandai_type enum.

---
+++ types/sandai_type.ts
@@ -0,0 +1,19 @@
+enum SandaiType {
+  CASTLE,
+  NIGHT_VIEW,
+  GARDEN,
+  VIEW,
+  GASSHO,
+  ONSEN,
+  SHRINE,
+  FESTIVAL,
+  FIREWORK,
+  TENMANGU,
+  CHINA_TOWN,
+  RED_LIGHT,
+  TEA,
+  WAGYU,
+  RAMEN,
+}
+
+export default SandaiType;
