3449bef4034ed5a75a2c9da811c076470fc23637
austinlaurice <austinlaurice@yahoo.com.tw>
2023-06-24 15:15:24 +0800

    Add color

--- utils/sandai_types_util.ts
+++ utils/sandai_types_util.ts
@@ -28,26 +28,37 @@ export function getIconName(type: SandaiType) {
 
 export function getColor(type: SandaiType) {
   switch (type) {
-    case SandaiType.CASTLE:
-      return "yellow";
-    case SandaiType.VIEW:
-      return "green";
-    case SandaiType.ONSEN:
-      return "blue";
-    case SandaiType.NIGHT_VIEW:
-    case SandaiType.GARDEN:
-    case SandaiType.GASSHO:
-    case SandaiType.SHRINE:
-    case SandaiType.FESTIVAL:
-    case SandaiType.FIREWORK:
-    case SandaiType.TENMANGU:
-    case SandaiType.CHINA_TOWN:
-    case SandaiType.RED_LIGHT:
-    case SandaiType.TEA:
     case SandaiType.WAGYU:
+      return "#d5a6ad"
+    case SandaiType.CHINA_TOWN:
+      return "#d3818a"
+    case SandaiType.RED_LIGHT:
+      return "#e5c3d1"
+    case SandaiType.GASSHO:
+      return "#e1a565";
+    case SandaiType.CASTLE:
+      return "#bf9f94";
     case SandaiType.RAMEN:
+      return "#f6dfad"
+    case SandaiType.VIEW:
+      return "#a5d0c6";
+    case SandaiType.GARDEN:
+      return"#059296"
+    case SandaiType.TEA:
+      return "#2b9d8a"
+    case SandaiType.ONSEN:
+      return "#3c81c6"
+    case SandaiType.NIGHT_VIEW:
+      return "#155d99"
+    case SandaiType.SHRINE:
+      return "#6463b5"
+    case SandaiType.FIREWORK:
+      return "#787cd7"
+    case SandaiType.TENMANGU:
+      return "#c2b4e9"
+    case SandaiType.FESTIVAL:
+      return "#9e6085"
     default:
-      // TODO: Add colors.
       return "blue";
   }
 }
