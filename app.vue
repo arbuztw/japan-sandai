08422b68bfd1927caa17519e1f96785baac23964
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2023-06-25 12:26:53 +0900

    Add map to main page.

--- app.vue
+++ app.vue
@@ -15,5 +15,20 @@ const spotVisits = new SpotVisit();
 <template>
   <div>
     <progress-bar :sandai-visits="spotVisits.sandaiVisits"></progress-bar>
+    <japan-map
+      :spot-visits="spotVisits.visited"
+      :prefectur-visits="spotVisits.prefectureVisits"
+    ></japan-map>
+    <div v-for="sandaiVisit in spotVisits.sandaiVisits">
+      <h3>{{ sandaiVisit.sandaiType }}</h3>
+      <div v-for="spotAttr in sandaiVisit.required_spots">
+        <input type="checkbox" v-model="spotAttr.visited.value" />
+        {{ spotAttr.spot }}
+      </div>
+      <div v-for="spotAttr in sandaiVisit.optional_spots">
+        <input type="checkbox" v-model="spotAttr.visited.value" />
+        {{ spotAttr.spot }}
+      </div>
+    </div>
   </div>
 </template>
