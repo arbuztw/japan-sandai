fc9aa5a7f0c4d267669359cb1ef5a649271c3449
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2023-12-15 22:02:31 +0900

    Showcase

--- app.vue
+++ app.vue
@@ -11,10 +11,28 @@ const showModal = ref(false);
 <template>
   <div id="page-container">
     <div id="map-container">
-      <japan-map
-        :spot-visits="spotVisits.visited"
-        :prefectur-visits="spotVisits.prefectureVisits"
-      ></japan-map>
+      <div id="main-container">
+        <div id="result">
+          <div class="result-title">全制霸</div>
+          <div class="result-content">
+            三大拉麵 三大拉麵<br />
+            三大拉麵 三大拉麵<br />
+            三大拉麵 三大拉麵<br />
+            三大拉麵 三大拉麵<br />
+            三大拉麵 三大拉麵<br />
+            三大拉麵 三大拉麵<br />
+            三大拉麵 三大拉麵<br />
+            三大拉麵<br />
+          </div>
+          <div class="result-title">制霸進度: 87% 不能再高了</div>
+        </div>
+        <japan-map
+          :spot-visits="spotVisits.visited"
+          :prefectur-visits="spotVisits.prefectureVisits"
+          @click="showModal = true"
+          style="cursor: pointer"
+        ></japan-map>
+      </div>
     </div>
     <div id="progress-bar-container">
       <progress-bar :sandai-visits="spotVisits.sandaiVisits"></progress-bar>
@@ -38,5 +56,22 @@ const showModal = ref(false);
 #map-container {
   flex-grow: 1;
   min-height: 0;
+}
+#main-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 100%;
+}
+#result {
+  padding: 0 5em;
+  text-align: center;
+}
+.result-title {
+  margin-top: 5rem;
+  font-size: 25pt;
+}
+.result-content {
+  font-size: 20pt;
 }
 </style>
